e9b60649fd79baf8d851b68eaaac3eedb9b03a51
Neal Bazirake <mckenzo12@live.com>
2015-06-16 20:57:13 -0400

    basicfile.py
    Our first fleshed out python code and probable functions to be used.

---
+++ basicfile.py
@@ -0,0 +1,42 @@
+
+
+'''
+	Date : 06/11/2015
+	Description : This will be a a relatively simple document breaking down the city_bash project
+
+'''
+##Necessary modules??
+
+import openpyxl
+import csv
+import os
+
+
+class CityBash(object):
+
+	def __init__(self):
+		pass
+
+
+	def pull_csv(self):
+
+		'''
+		Pull CSV data into python and seperate the fields by variables
+		'''
+
+		os.chdir("//JSELECTED PATH") # Depending on where we'll be pulling this from
+     	wb = load_workbook('initial.xlsx')
+		pass
+
+
+	def get_geocoded_cordinates(self):
+		pass
+		
+
+
+def main():
+	print("###")
+
+	#Run the intial pull_csv and 
+
+main()
